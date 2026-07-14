e4786f2c348980624bec683457799259d0217aaf
Lopson <gmdlourenco@gmail.com>
2026-07-14 17:19:23 +0200

    Fixed force run value.

--- Configuration.ps1
+++ Configuration.ps1
@@ -1,7 +1,7 @@
 [string]$ANNOUNCERS_FOLDER = "Voices";
 [string]$ANNOUNCER_TO_USE = "fvox";
 [bool]$FOLLOW_THEME = $true;
-[bool]$FORCE_RUN = $true;
+[bool]$FORCE_RUN = $false;
 [bool]$READ_OUT_LOUD = $true;
 [bool]$READ_DURING_MEDIA_PLAYBACK = $false;
 [bool]$READ_DURING_VIDEOGAMES = $false;
